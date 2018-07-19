cda07caa5f2b63360a19a1355417bf0133875040
Emily Satterfield <esatterfield32@gmail.com>
2018-07-18 21:42:06 -0400

    change some names. add html stuff

--- contact.php
+++ contact.php
@@ -1,5 +1,5 @@
 <?php
-if(isset($_POST['submit'])){
+if(isset($_POST['send'])){
     $to = 'esatterfield32@gmail.com';
     $subject = "Website Contact";
     
@@ -7,10 +7,21 @@ if(isset($_POST['submit'])){
     $message .= 'Email: ' .$_POST['email'] . "\r\n\r\n";
     $message .= 'Message: ' .$_POST['message'] . "\r\n\r\n";
     
-    echo $message;
 }
+echo $message;
 ?>
-<h1>Thank you!</h1>
-<p>Your message has sent!</p>
-<h1>Woops!</h1>
-<p>There was a problem sending your message.</p>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset= "utf-8" />
+        <title>Message Sent</title>
+        <link rel = "stylesheet" type = "text/css" href = "style.css"/>
+        <meta name = "viewport" content = "width = device-width, inital -scale =1.0">
+    </head>
+    <body>
+        <h1>Thank you!</h1>
+        <p>Your message has sent!</p>
+        <h1>Woops!</h1>
+        <p>There was a problem sending your message.</p>
+    </body>
+</html>
